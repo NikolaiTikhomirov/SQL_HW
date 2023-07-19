a13fa9cee2645e13d43c443ec5c808a3dcd31898
Nikolai Tikhomirov <79159489230@yandex.ru>
2023-07-19 23:54:52 +0300

    HW4 complete

--- HW4/HW4.sql
+++ HW4/HW4.sql
@@ -9,14 +9,22 @@ JOIN profiles AS p ON u.id = p.user_id
 WHERE TIMESTAMPDIFF(YEAR, p.birthday, CURDATE()) < 12
 GROUP BY u.id;
 
-/* Определить кто больше поставил лайков (всего): мужчины или женщины. 
+/*
+Определить кто больше поставил лайков (всего): мужчины или женщины. 
+Я не нашел таблицы, где отображается информация о том, кто ставит лайки, по этому мой код высчитывает то,
+кому больше поставили лайков: мужчинам или женщинам.
 */
 
-SELECT T.gender, MAX(T.likecount) AS like_count
-FROM (SELECT p.gender, COUNT(l.user_id) AS likecount
+SELECT p.gender, COUNT(l.user_id) AS likecount
 FROM profiles AS p
 JOIN likes AS l ON p.user_id = l.user_id
-GROUP BY p.gender) AS T;
+GROUP BY p.gender
+ORDER BY likecount DESC LIMIT 1;
 
-/* Вывести всех пользователей, которые не отправляли сообщения.
-(по желанию)* Пусть задан некоторый пользователь. Из всех друзей этого пользователя найдите человека, который больше всех написал ему сообщений. */
+/* Вывести всех пользователей, которые не отправляли сообщения. */
+
+SELECT id, firstname, lastname
+FROM users
+WHERE id NOT IN (SELECT from_user_id FROM messages);
+
+/* (по желанию)* Пусть задан некоторый пользователь. Из всех друзей этого пользователя найдите человека, который больше всех написал ему сообщений. */
